c594db5e8a4c704dbc809172c6b286dcfcb68a44
Maik Hummel <yo@beevelop.com>
2015-04-04 17:26:30 +0200

    integrate grunt

---
+++ GruntFile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        // Update config.files, commit and push
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
+
+    grunt.registerTask('release', ['bump-commit']);
+    grunt.registerTask('release-patch', ['bump-only:patch', 'release']);
+    grunt.registerTask('release-minor', ['bump-only:minor', 'release']);
+    grunt.registerTask('release-major', ['bump-only:major', 'release']);
+    grunt.registerTask('prerelease', ['bump-only:prerelease', 'release']);
+};
